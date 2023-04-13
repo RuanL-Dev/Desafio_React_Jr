093fa756c65b75a9bf50f3a8ec6cb45ba99cc53b
RuanL-Dev <ruanlopes21@gmail.com>
2023-04-12 22:27:52 -0300

    Changing font to Inter

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,7 +10,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Inter', sans-serif;
     background-color: ${(props) => props.theme.background};
   }
 `
