6fc19d3beec8524fd4aba7e6467b680a389b3881
RuanL-Dev <ruanlopes21@gmail.com>
2023-04-14 20:47:00 -0300

    newProduct page created

---
+++ pages/newProduct.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+import { joiResolver } from '@hookform/resolvers/joi'
+import axios from 'axios'
+
+import styled from 'styled-components'
+
+import Navbar from '../src/components/navbar/Navbar'
+import ContainerPage from '../src/components/layout/ContainerPage'
+import Body from '../src/components/layout/Body'
+import UseContainer from '../src/components/layout/UserContainer'
+import Input from '../src/components/Input/Input'
+import GeneralButton from '../src/components/button/GeneralButton'
+
+const StyledText = styled.h1`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  padding-top: 10px;
+  color: ${(props) => props.theme.WhiteBackground};
+`
+
+const FormContainer = styled.div`
+  margin: 15vh 0;
+  position: relative;
+`
+const ContainerButtonSave = styled.div`
+  position: absolute;
+  right: 250px;
+  bottom: -70px;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 600px;
+  margin-right: 100px;
+`
+
+const StyledInputDescription = styled.div`
+  margin: 10px;
+`
+
+function newProduct() {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid }
+  } = useForm({
+    resolver: joiResolver(),
+    mode: 'all'
+  })
+  return (
+    <Body>
+      <Navbar />
+      <UseContainer />
+      <ContainerPage>
+        <StyledText>Cadastre seu novo produto</StyledText>
+        <FormContainer>
+          <Form>
+            <Input label="Código" placeholder="Insira o código" name="Code" control={control} />
+            <Input label="Preço" placeholder="Informe o preço" name="Price" control={control} />
+            <Input
+              label="Data do cadastro"
+              placeholder="(DD/MM/YYYY)"
+              name="Date"
+              control={control}
+            />
+            <StyledInputDescription>
+              <Input
+                label="Descrição"
+                placeholder="Insira uma descrição"
+                name="Description"
+                padding="70"
+                control={control}
+              />
+            </StyledInputDescription>
+            <ContainerButtonSave>
+              <GeneralButton>SALVAR</GeneralButton>
+            </ContainerButtonSave>
+          </Form>
+        </FormContainer>
+      </ContainerPage>
+    </Body>
+  )
+}
+
+export default newProduct
