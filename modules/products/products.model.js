11f61c4d2dfa5bfa0088829dda147e8e8f926a79
RuanL-Dev <ruanlopes21@gmail.com>
2023-04-15 12:45:22 -0300

    Description now needs to be unique

--- modules/products/products.model.js
+++ modules/products/products.model.js
@@ -4,7 +4,7 @@ const NewProductSchema = new mongoose.Schema({
   Code: { type: String, required: true, uppercase: true, maxlength: 10 },
   Price: { type: String, required: true, maxlength: 8 },
   Date: { type: String, required: true, maxlength: 11 },
-  Description: { type: String, required: true, uppercase: true, maxlength: 70 },
+  Description: { type: String, required: true, uppercase: true, maxlength: 70, unique: true },
   isLiked: { type: Boolean }
 })
 
