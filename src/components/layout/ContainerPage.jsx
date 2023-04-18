1edc1bed568f09a4a07d1d8e6ca3b5d627d67599
RuanL-Dev <ruanlopes21@gmail.com>
2023-04-18 20:45:22 -0300

    adding margim bottom to ContainerPage

--- src/components/layout/ContainerPage.jsx
+++ src/components/layout/ContainerPage.jsx
@@ -4,6 +4,7 @@ const StyledMainContainer = styled.div`
   background-color: ${(props) => props.theme.secondBackgroundColor};
   width: 95%;
   min-height: 100vh;
+  margin-bottom: 2rem;
   display: flex;
   align-items: center;
   flex-direction: column;
